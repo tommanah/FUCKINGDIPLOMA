22cefa4e0e06af488062902b3e7395cff572a628
tommanah <mirtomma@gmail.com>
2025-05-20 16:50:27 +0300

    fixik

--- server.js
+++ server.js
@@ -2,16 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
-const cors = require('cors');
-const path = require('path');
-
 
 const app = express();
-app.use(cors());
 app.use(express.json()); // для чтения JSON в теле запроса
-
-// Подключаем маршруты авторизации
-app.use('/api/auth', authRoutes);
 
 // Подключаемся к MongoDB
 mongoose.connect(process.env.MONGO_URI, {
@@ -20,13 +13,8 @@ mongoose.connect(process.env.MONGO_URI, {
 }).then(() => console.log('✅ MongoDB подключена'))
   .catch((err) => console.error('❌ Ошибка подключения к MongoDB:', err));
 
-// Указываем путь к собранному фронтенду
-app.use(express.static(path.join(__dirname, 'front-container/build')));
+// Подключаем маршруты авторизации
+app.use('/api/auth', authRoutes);
 
-// Для всех остальных маршрутов отдаем индексный файл React
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'front-container/build', 'index.html'));
-});
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`🚀 Сервер запущен на порту ${PORT}`));
